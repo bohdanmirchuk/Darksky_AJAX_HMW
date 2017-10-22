eb9ca09b8592321764ad846b1dd98022073cd4cc
Yuriy_Kovalchuk <Yuriy.Kovalchuk@symphony-solutions-eu>
2017-10-22 17:47:25 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+(function(){
+
+  function init(){
+    getLocation();
+  };
+
+  function getLocation(){
+    var deffered = $.Deferred();
+    if (navigator.getLocation){
+      navigator.getLocation.getCurrentPosition(function(data){
+        console.log(data);
+        deferred.resolve(data);
+      },
+      function(error){
+        console.log(error);
+        deferred.reject (data);
+      });
+    }
+
+    return deffered.promise();
+  };
+
+  init();
+})()
+// var url1 = "https://randomuser.me/api/?result=2";
+// var url2 =  "https://api.darksky.net/forecast/ec7ca3493d508e807cfe8300fac7ba35/37.8267,-122.4233";
+
+
+// fetch(url2)
+//   .then(function(response) {
+// return response.json();
+
+//    })
+//   .then(function(response){
+//     response.results.forEach(function(result){
+//       console.log(result.name.first);
+//     })
+//     console.log(response.results[0].name.first);
+//     console.log(response);
+//   })
+  // .catch( alert("shit") );
+
+//   fetch('https://api.darksky.net/forecast/ec7ca3493d508e807cfe8300fac7ba35/37.8267,-122.4233')
+//   .then(function(response) {
+// return response.json();
+
+//    })
+//   .then(function(response){
+//     response.results.forEach(function(result){
+//       console.log(result.currently.time);
+//     })
+//     console.log(response.results[0].name.first);
+//   })
+//   // .catch( alert("shit") );
