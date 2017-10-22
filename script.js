efd3a9001120afbd1b2d6e36f7c7dedad1db9d6b
Yuriy_Kovalchuk <Yuriy.Kovalchuk@symphony-solutions-eu>
2017-10-22 20:01:11 +0300

    efwe

--- script.js
+++ script.js
@@ -25,15 +25,23 @@
 var url1 = "https://randomuser.me/api/?result=2";
 var url2 =  "https://api.darksky.net/forecast/ec7ca3493d508e807cfe8300fac7ba35/37.8267,-122.4233";
 
-
-fetch(url2)
+// $.ajax({
+//     type: 'GET',
+//     url: url2,
+//     dataType: 'jsonp',
+//   }).done(function(response) {
+//     console.log(response);
+//   })
+fetch(url1,  {mode: 'no-cors',
+             method: 'GET',
+             headers: 'Access-Control-Allow-Origin: *'})
   .then(function(response) {
 return response.json();
-
    })
   .then(function(response){
     response.results.forEach(function(result){
-      console.log(result.name.first);
+      console.log(response.headers);
+      console.log(result.currently.time);
     })
     console.log(response.results[0].name.first);
     console.log(response);
