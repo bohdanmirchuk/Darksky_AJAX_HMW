fb71fc79357dcb532e0409aa5653f35d522e54f9
Yuriy_Kovalchuk <Yuriy.Kovalchuk@symphony-solutions-eu>
2017-10-23 15:46:20 +0300

    getWeather added

--- script.js
+++ script.js
@@ -23,32 +23,20 @@ var userPositionPromise = getCurrentPosition();
 userPositionPromise
   .then(function(data) {
    latitude = data.coords.latitude;
-    longitude = data.coords.longitude;
-    console.log('latitude', latitude, 'longitude', longitude);
+   longitude = data.coords.longitude;
+   console.log('latitude', latitude, 'longitude', longitude);
   getLocality();
+  getWeather();
   })
   .fail(function(error) {
     console.warn('Something wrong...Error'+error.code+':'+error.message);
   });
 
 
-function geoFindMe() {
-  var output = document.getElementById("out");
-  
-   if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    return;
-  }
-   output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-   output.insertAdjacentHTML('beforeEnd', '<p id="locality">'+locality+'</p>');
-     output.insertAdjacentHTML('beforeEnd', '<p id="date">'+moment.unix(date).format('dddd Do')+'<br>'+moment.unix(date).format('HH:mm')+'</p>');
-
-  
+function geoFindMe() {  
     var img = new Image();
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=12&size=300x300&key=AIzaSyDGrTQNh1fmVkol36vbLQQppvqRwAExpDE";
-    
-    getWeather();
-    output.appendChild(img);
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=10&size=100x100&key=AIzaSyDGrTQNh1fmVkol36vbLQQppvqRwAExpDE";
+    $('#map').append(img);
 }
 
 function getWeather(){
@@ -60,7 +48,13 @@ var url = 'https://api.darksky.net/forecast/ec7ca3493d508e807cfe8300fac7ba35/'+l
   }).done(function(response) {
     console.log(response.currently.icon);
     var icon = response.currently.icon;
+    $('#locality').text(locality);
+    $('#date').text(moment.unix(date).format("dddd Do"));
+    geoFindMe();
     skyconsIcon(icon);
+    $('#summary').text(response.currently.summary);
+    var temp = response.currently.temperature;
+    $('#temperature').text(Math.floor((temp-32)*5/9)+'°C');
   })
 }
 
@@ -81,7 +75,7 @@ var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+'
 }
 
 function skyconsIcon(icon){
- var skycons = new Skycons({"color": "black"}); 
+ var skycons = new Skycons({"color": "white"}); 
  skycons.add("icon", icon);
  skycons.play();
 }
